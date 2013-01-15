988b402ecac9c552c402464dd04fe843e5c240fb
serhand <serhand@gmail.com>
2013-01-15 16:48:44 +0200

    update denemesi

--- Demographics Simulator/src/com/servisim/simulator/map/WeightedRandomCollection.java
+++ Demographics Simulator/src/com/servisim/simulator/map/WeightedRandomCollection.java
@@ -4,7 +4,7 @@ public class WeightedRandomCollection<E> {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hebele");
+		System.out.println("hebeffle");
 	}
 
 }
