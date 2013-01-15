fb5652088029e60084bf37f503aed1b4bed198b1
serhand <serhand@gmail.com>
2013-01-15 16:24:50 +0200

    initial commit

---
+++ Demographics Simulator/src/com/servisim/simulator/map/WeightedRandomCollection.java
@@ -0,0 +1,10 @@
+package com.servisim.simulator.map;
+
+public class WeightedRandomCollection<E> {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hebele");
+	}
+
+}
