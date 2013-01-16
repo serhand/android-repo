9eac14c15970ec61d407cc395c77cf2271db1364
serhand <serhand@gmail.com>
2013-01-16 16:58:42 +0200

    .

--- Demographics Simulator/src/com/servisim/simulator/gui/GridMap.java
+++ Demographics Simulator/src/com/servisim/simulator/gui/GridMap.java
@@ -1,19 +1,56 @@
 package com.servisim.simulator.gui;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 
 public class GridMap extends JComponent {
+	Image i = null;
+	Dimension grid = null;
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 5004568091337606248L;
-	
+
+	public GridMap() {
+		try {
+			i = ImageIO.read(new File("resources/ist_population_density.bmp"));
+		} catch (IOException e) {
+			System.out.println("Error reading image : " + e.getMessage());
+		}
+	}
+
 	@Override
 	public Dimension getPreferredSize() {
 		return new Dimension(500, 300);
+	}
+
+	public void setGrid(Dimension d) {
+		if (grid == null || (grid != null && !grid.equals(d))) {
+			grid = d;
+			repaint();
+		}
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		if (i != null)
+			g.drawImage(i, 0, 0, null);
+		if (grid != null) {
+			final int height = getHeight();
+			final int width = getWidth();
+			for (int i = 0; i <= height; i += grid.height)
+				g.drawLine(0, i, width, i);
+			for (int i = 0; i <= width; i += grid.width)
+				g.drawLine(i, 0, i, height);
+		}
 	}
 
 }
--- Demographics Simulator/src/com/servisim/simulator/main/Simulator.java
+++ Demographics Simulator/src/com/servisim/simulator/main/Simulator.java
@@ -1,7 +1,17 @@
 package com.servisim.simulator.main;
 
+import javax.swing.BorderFactory;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+
 import com.servisim.simulator.gui.GridMap;
 
 public class Simulator {
@@ -9,12 +19,36 @@ public class Simulator {
 	private static void createAndShowGUI() {
 		JFrame frame = new JFrame("Demographics Simulator");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Add the ubiquitous "Hello World" label.
-        JComponent component = new GridMap();
-        frame.getContentPane().add(component);
+
+		JPanel mapPane = new JPanel(new BorderLayout());
+		mapPane.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createTitledBorder("Density Profile"),
+                BorderFactory.createEmptyBorder(5,5,5,5)));
+		final GridMap gridMap = new GridMap();
+		gridMap.setGrid(new Dimension(10,10));
+        JPanel cellSizePane = new JPanel(new BorderLayout());
+        cellSizePane.setBorder(BorderFactory.createTitledBorder("Cell width in pixels"));
+        
+        JSlider cellSizer = new JSlider(10, 100, 10);
+        cellSizer.setSnapToTicks(true);
+        cellSizer.setMajorTickSpacing(10);
+        cellSizer.setPaintTicks(true);
+        cellSizer.setPaintLabels(true);
+        cellSizer.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				int size = ((JSlider)e.getSource()).getValue();
+				gridMap.setGrid(new Dimension(size,size));
+				
+			}
+		});
+
+        cellSizePane.add(cellSizer);
+        
+        mapPane.add(cellSizePane,BorderLayout.SOUTH);
+        mapPane.add(gridMap,BorderLayout.CENTER);
  
-        //Display the window.
+        frame.getContentPane().add(mapPane);
         frame.pack();
         frame.setVisible(true);
 	}
