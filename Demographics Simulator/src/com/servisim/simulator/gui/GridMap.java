b199c2a665637f997f2c1b85df3391911ef89e19
serhand <serhand@gmail.com>
2013-01-16 10:26:47 +0200

    base

---
+++ Demographics Simulator/src/com/servisim/simulator/gui/GridMap.java
@@ -0,0 +1,12 @@
+package com.servisim.simulator.gui;
+
+import javax.swing.JComponent;
+
+public class GridMap extends JComponent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5004568091337606248L;
+
+}
---
+++ Demographics Simulator/src/com/servisim/simulator/utils/WeightedRandomCollection.java
@@ -0,0 +1,32 @@
+package com.servisim.simulator.utils;
+
+import java.util.NavigableMap;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class WeightedRandomCollection<E> {
+	private final NavigableMap<Double, E> map = new TreeMap<Double, E>();
+	private final Random random;
+	private double total = 0;
+
+	public WeightedRandomCollection() {
+		this(new Random());
+	}
+
+	public WeightedRandomCollection(Random random) {
+		this.random = random;
+	}
+
+	public void add(double weight, E result) {
+		if (weight <= 0)
+			return;
+		total += weight;
+		map.put(total, result);
+	}
+
+	public E next() {
+		double value = random.nextDouble() * total;
+		return map.ceilingEntry(value).getValue();
+	}
+
+}
