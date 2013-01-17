a90f6e0e76897b946979ba0e9ec1b91809b1eea3
serhand <serhand@gmail.com>
2013-01-17 16:55:41 +0200

    .

--- Demographics Simulator/src/com/servisim/simulator/gui/utilities/SelectableLegendImage.java
+++ Demographics Simulator/src/com/servisim/simulator/gui/utilities/SelectableLegendImage.java
@@ -1,20 +1,25 @@
 package com.servisim.simulator.gui.utilities;
 
-import java.awt.BorderLayout;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-import javax.swing.BoxLayout;
 import javax.swing.JComponent;
+import javax.swing.RepaintManager;
 
-public class SelectableLegendImage extends JComponent implements MouseListener {
+public class SelectableLegendImage extends JComponent implements MouseListener, MouseMotionListener {
 	private BufferedImage legendImage = null;
+	private Rectangle selectionRect = new Rectangle();
+	private Point pointOfClick = new Point();
+	private Point pointOfDrag = new Point();
 
 	public SelectableLegendImage() {
 		try {
@@ -24,6 +29,7 @@ public class SelectableLegendImage extends JComponent implements MouseListener {
 			System.out.println("Error reading legend : " + e.getMessage());
 		}
 		addMouseListener(this);
+		addMouseMotionListener(this);
 	}
 
 	@Override
@@ -33,12 +39,24 @@ public class SelectableLegendImage extends JComponent implements MouseListener {
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		if (legendImage != null)
-			g.drawImage(legendImage, 0, 0, null);
+		if (legendImage != null){
+			Rectangle clipRect = g.getClipBounds();
+			final int x1 = clipRect.x;
+			final int y1 = clipRect.y;
+			final int x2 = x1 + clipRect.width;
+			final int y2 = y1 + clipRect.height;
+			g.drawImage(legendImage,x1,y1,x2,y2,x1,y1,x2,y2,null);
+			Rectangle selection = new Rectangle(pointOfClick);
+			selection.add(pointOfDrag);
+			g.drawRect(selection.x, selection.y, selection.width, selection.height);
+		}
+		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		pointOfClick.x = e.getX();
+		pointOfClick.y = e.getY();
 		System.out.println("clicked");
 	}
 
@@ -50,6 +68,8 @@ public class SelectableLegendImage extends JComponent implements MouseListener {
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		System.out.println("released");
+		pointOfDrag.x = e.getX();
+		pointOfDrag.y = e.getY();
 	}
 
 	@Override
@@ -62,5 +82,21 @@ public class SelectableLegendImage extends JComponent implements MouseListener {
 	public void mouseExited(MouseEvent e) {
 		setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		System.out.println("exited");
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		pointOfDrag.x = e.getX();
+		pointOfDrag.y = e.getY();
+		Rectangle clip = new Rectangle(pointOfClick);
+		clip.add(pointOfDrag);
+		repaint();
+		//RepaintManager.currentManager(this).addDirtyRegion(this, clip.x, clip.y, clip.width, clip.height);
+		System.out.println("dragged " + e.getX() + " " + e.getY());
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
 	}
 }
